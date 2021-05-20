3b638ce70d730c79c4ae469dc0ee33001d4c4e42
vernessastone <84538110+vernessastone@users.noreply.github.com>
2021-05-20 17:41:20 -0400

    Beginner name program
    This program displays my name.

---
+++ Github 1.py
@@ -0,0 +1,13 @@
+# Greets/displays full name 
+
+n1 = "vernessa"
+# first name
+n2 = "stone"
+# last name
+n3 = f"{n1} {n2}"
+#both names together 
+pm= f"{n3.title()}"
+# calls title method 
+print ("Good evening,\n" , pm)
+# displays name
+# + startes newline
