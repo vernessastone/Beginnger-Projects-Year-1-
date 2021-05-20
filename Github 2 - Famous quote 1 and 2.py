c6faecdece71ab40471ddb479468b19a703fad9f
vernessastone <84538110+vernessastone@users.noreply.github.com>
2021-05-20 19:55:40 -0400

    Famous name quote
    MLK - my hero

---
+++ Github 2 - Famous quote 1 and 2.py
@@ -0,0 +1,12 @@
+# this program display a famous
+# quote
+
+q1 = "martin luther king"
+# name  
+q2 = """In the end, we will remember \
+not the words of our enemies,
+but the silence of our friends. """
+# quote
+print (q1.title() , "once said,\n" , q2.strip())
+# prints name + calles title
+# and strip method
