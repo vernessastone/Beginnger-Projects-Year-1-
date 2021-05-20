4730bf873de9d7c0f1e217263c1c269d98d50b92
vernessastone <84538110+vernessastone@users.noreply.github.com>
2021-05-20 18:40:28 -0400

    Famous quote

---
+++ Github 2 - Famous quote 1 and 2.py
@@ -0,0 +1,119 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========== RESTART: C:/Users/Vernessa/Documents/py werk/Github 1.py =========
+Traceback (most recent call last):
+  File "C:/Users/Vernessa/Documents/py werk/Github 1.py", line 3, in <module>
+    n1 = Vernessa # first name
+NameError: name 'Vernessa' is not defined
+>>> 
+========== RESTART: C:/Users/Vernessa/Documents/py werk/Github 1.py =========
+Traceback (most recent call last):
+  File "C:/Users/Vernessa/Documents/py werk/Github 1.py", line 3, in <module>
+    n = Vernessa # first name
+NameError: name 'Vernessa' is not defined
+>>> 
+========== RESTART: C:/Users/Vernessa/Documents/py werk/Github 1.py =========
+Traceback (most recent call last):
+  File "C:/Users/Vernessa/Documents/py werk/Github 1.py", line 3, in <module>
+    n= Vernessa # first name
+NameError: name 'Vernessa' is not defined
+>>> 
+>>> 
+========== RESTART: C:/Users/Vernessa/Documents/py werk/Github 1.py =========
+Traceback (most recent call last):
+  File "C:/Users/Vernessa/Documents/py werk/Github 1.py", line 3, in <module>
+    n1 = Vernessa
+NameError: name 'Vernessa' is not defined
+>>> 
+========== RESTART: C:/Users/Vernessa/Documents/py werk/Github 1.py =========
+Traceback (most recent call last):
+  File "C:/Users/Vernessa/Documents/py werk/Github 1.py", line 3, in <module>
+    n1 = Vernessa
+NameError: name 'Vernessa' is not defined
+>>> 
+========== RESTART: C:/Users/Vernessa/Documents/py werk/Github 1.py =========
+Traceback (most recent call last):
+  File "C:/Users/Vernessa/Documents/py werk/Github 1.py", line 3, in <module>
+    Vernessa = n1
+NameError: name 'n1' is not defined
+>>> 
+========== RESTART: C:/Users/Vernessa/Documents/py werk/Github 1.py =========
+(n3.title())
+>>> 
+========== RESTART: C:/Users/Vernessa/Documents/py werk/Github 1.py =========
+(n3.title())
+>>> 
+========== RESTART: C:/Users/Vernessa/Documents/py werk/Github 1.py =========
+(n3.title())
+>>> 
+========== RESTART: C:/Users/Vernessa/Documents/py werk/Github 1.py =========
+Traceback (most recent call last):
+  File "C:/Users/Vernessa/Documents/py werk/Github 1.py", line 9, in <module>
+    pm= (n3.title())
+AttributeError: 'tuple' object has no attribute 'title'
+>>> 
+========== RESTART: C:/Users/Vernessa/Documents/py werk/Github 1.py =========
+('Vernessa', 'Stone')
+>>> 
+========== RESTART: C:/Users/Vernessa/Documents/py werk/Github 1.py =========
+Traceback (most recent call last):
+  File "C:/Users/Vernessa/Documents/py werk/Github 1.py", line 9, in <module>
+    pm= f"{n3,title()}"
+NameError: name 'title' is not defined
+>>> 
+========== RESTART: C:/Users/Vernessa/Documents/py werk/Github 1.py =========
+Hey Vernessa , Stone
+>>> 
+========== RESTART: C:/Users/Vernessa/Documents/py werk/Github 1.py =========
+Hey Vernessastone
+>>> 
+========== RESTART: C:/Users/Vernessa/Documents/py werk/Github 1.py =========
+Hey Vernessa Stone
+>>> 
+========== RESTART: C:/Users/Vernessa/Documents/py werk/Github 1.py =========
+Good evenig, Vernessa Stone
+>>> 
+========== RESTART: C:/Users/Vernessa/Documents/py werk/Github 1.py =========
+Good evening, Vernessa Stone
+>>> 
+========== RESTART: C:/Users/Vernessa/Documents/py werk/Github 1.py =========
+Good evening, 
+ Vernessa Stone
+>>> 
+========== RESTART: C:\Users\Vernessa\Documents\py werk\Github 1.py =========
+Good evening,
+ Vernessa Stone
+>>> 
+= RESTART: C:/Users/Vernessa/Documents/py werk/Github 2 - Famous quote 1 and 2.py
+Martin Luther King , In The End, We Will Remember Not The Words Of Our Enemies, But The Silence Of Our Friends.
+>>> 
+= RESTART: C:/Users/Vernessa/Documents/py werk/Github 2 - Famous quote 1 and 2.py
+Martin Luther King Once said,
+>>> 
+= RESTART: C:/Users/Vernessa/Documents/py werk/Github 2 - Famous quote 1 and 2.py
+Martin Luther King once said,
+>>> 
+= RESTART: C:/Users/Vernessa/Documents/py werk/Github 2 - Famous quote 1 and 2.py
+Martin luther king once said,
+>>> 
+= RESTART: C:/Users/Vernessa/Documents/py werk/Github 2 - Famous quote 1 and 2.py
+Martin luther king.title() once said,
+>>> 
+= RESTART: C:/Users/Vernessa/Documents/py werk/Github 2 - Famous quote 1 and 2.py
+Martin Luther King once said,
+>>> 
+= RESTART: C:/Users/Vernessa/Documents/py werk/Github 2 - Famous quote 1 and 2.py
+Martin Luther King once said,
+>>> 
+= RESTART: C:/Users/Vernessa/Documents/py werk/Github 2 - Famous quote 1 and 2.py
+Martin Luther King once said,/n/t In the end, we will remember not the words of our enemies, but the silence of our friends.
+>>> 
+= RESTART: C:/Users/Vernessa/Documents/py werk/Github 2 - Famous quote 1 and 2.py
+Martin Luther King once said,
+	 In the end, we will remember not the words of our enemies, but the silence of our friends.
+>>> 
+= RESTART: C:/Users/Vernessa/Documents/py werk/Github 2 - Famous quote 1 and 2.py
+Martin Luther King once said,
+ In the end, we will remember not the words of our enemies, but the silence of our friends.
+>>> 
